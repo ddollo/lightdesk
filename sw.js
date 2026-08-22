726d46b8f5929f4faf734c30ce164b6f4b0e8a5d
ddollo <137148083+ddollo@users.noreply.github.com>
2026-08-22 17:02:24 +0900

    Remove login and sync widget selections

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lightdesk-shell-v5";
+const CACHE = "lightdesk-shell-v6";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
