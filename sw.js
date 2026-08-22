40d58b9cdfa41a6f5d0c85be92d771f34c7afd04
ddollo <137148083+ddollo@users.noreply.github.com>
2026-08-22 19:23:52 +0900

    Fix task creation without session

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lightdesk-shell-v6";
+const CACHE = "lightdesk-shell-v7";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
