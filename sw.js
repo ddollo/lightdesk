a63176e179a5be5dceeed18cc4c7a37474b7832b
ddollo <137148083+ddollo@users.noreply.github.com>
2026-08-22 16:35:52 +0900

    Use simple ID and password login

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lightdesk-shell-v4";
+const CACHE = "lightdesk-shell-v5";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
